37a626758f5de9be61bb480cbce26467fdcddfaa
Irma Octavia <irmaoctavia36@gmail.com>
2026-08-03 22:10:11 +0700

    Update dashboard dan dataset IndoBERT

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # ==========================================================
 # LOAD DATA
 # ==========================================================
-DATA_PATH = "data/dataset_final.csv"
+DATA_PATH = "data/dataset_final_indobert.csv"
 
 @st.cache_data
 def load_data():
